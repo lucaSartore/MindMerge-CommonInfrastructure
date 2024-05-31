b63cfe10968105548d94805dd227dab9401192dd
Luca Sartore <lucasar.com8@gmail.com>
2024-05-31 16:20:43 +0200

    Add name to task tree

--- task_tree.js
+++ task_tree.js
@@ -3,15 +3,18 @@
  * @type {Object}
  * @property {number} taskId - The id of the task
  * @property {TaskTree[]} childTasks - The child tasks of the task
+ * @property {string} taskName - The name of the task
  */
 class TaskTree{
 
     /**
      * @param {number} taskId 
+     * @param {string} taskName
     */
-    constructor(taskId){
+    constructor(taskId,taskName){
         this.taskId = taskId;
         this.childTasks = [];
+        this.taskName = taskName;
     }
 }
 
